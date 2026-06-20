86fc63bdab9e3e267fe3a2254407dc5d81ff6f0c
eyob <eyob.tadesse@astu.edu.et>
2026-06-20 07:18:57 +0300

    initial commit

---
+++ src/main/java/com/app/ecommerce/User.java
@@ -0,0 +1,8 @@
+package com.app.ecommerce;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/main/java/com/app/ecommerce/UserController.java
@@ -0,0 +1,9 @@
+package com.app.ecommerce;
+
+import java.util.List;
+
+public class UserController {
+    public List<User> getAllUsers(){
+        return ;
+    }
+}
