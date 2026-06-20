8ad061de7e11fcf2215bc86b97bd1c004fb64529
eyob <eyob.tadesse@astu.edu.et>
2026-06-20 08:20:53 +0300

    adding new user

--- src/main/java/com/app/ecommerce/UserController.java
+++ src/main/java/com/app/ecommerce/UserController.java
@@ -1,9 +1,23 @@
 package com.app.ecommerce;
 
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+@RestController
 public class UserController {
-    public List<User> getAllUsers(){
-        return ;
+    private List<User> userList = new ArrayList<>();
+
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        return userList;
+    }
+    @PostMapping("/api/users")
+    public List<User> CreateUsers(@RequestBody User user) {
+        userList.add(user);
+        return userList;
     }
 }
