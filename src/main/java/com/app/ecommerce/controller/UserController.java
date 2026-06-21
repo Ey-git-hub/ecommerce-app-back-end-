37b06caf2304f903432e28203c538910d8b03a82
eyob <eyob.tadesse@astu.edu.et>
2026-06-21 21:08:59 +0300

    dto is added to the project

--- src/main/java/com/app/ecommerce/controller/UserController.java
+++ src/main/java/com/app/ecommerce/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.app.ecommerce.controller;
 
-import com.app.ecommerce.model.User;
+import com.app.ecommerce.dto.UserRequests;
+import com.app.ecommerce.dto.UserResponse;
+
 import com.app.ecommerce.service.UserServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,13 +20,13 @@ public class UserController {
 
     @GetMapping
     //response entity make uniformitiy
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
 //        HttpStatus.
         return new ResponseEntity<>(userService.fetchAllUsers(),HttpStatus.OK);
     }
     @GetMapping("/{id}")
 //    @RequestMapping(value="/api/users/{id}",method=RequestMethod.GET)
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(()->ResponseEntity.notFound().build());
@@ -36,13 +38,13 @@ public class UserController {
 //        return new ResponseEntity<>(user,HttpStatus.OK);
     }
     @PostMapping
-    public ResponseEntity<String> CreateUsers(@RequestBody User user) {
-        userService.addUsers(user);
+    public ResponseEntity<String> CreateUsers(@RequestBody UserRequests userRequests) {
+        userService.addUsers(userRequests);
         return ResponseEntity.ok( "user added successfully");
     }
     @PutMapping("/{id}")
-    public ResponseEntity<String> UpdateUsers(@PathVariable Long id,@RequestBody User updatedUser) {
-        boolean updated=userService.UpdateUsers(updatedUser, id);
+    public ResponseEntity<String> UpdateUsers(@PathVariable Long id,@RequestBody UserRequests updatedUserRequest) {
+        boolean updated=userService.UpdateUsers(updatedUserRequest, id);
         if (updated){
             return ResponseEntity.ok( "user updated successfully");
 
--- src/main/java/com/app/ecommerce/service/UserServices.java
+++ src/main/java/com/app/ecommerce/service/UserServices.java
@@ -1,5 +1,9 @@
 package com.app.ecommerce.service;
 
+import com.app.ecommerce.dto.AddressDTO;
+import com.app.ecommerce.dto.UserRequests;
+import com.app.ecommerce.dto.UserResponse;
+import com.app.ecommerce.model.Address;
 import com.app.ecommerce.repository.UserRepository;
 import com.app.ecommerce.model.User;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,17 +21,18 @@ public class UserServices {
     //    private List<User> userList = new ArrayList<>();
 //    private Long nextid=1L;
     //adding the user
-    public void addUsers(User user) {
-
+    public void addUsers(UserRequests userRequests) {
+    User user = new User();
+    updateUserFromRequest(user,userRequests);
         userRepository.save(user);
     }
 
+
     //update the user
-    public boolean UpdateUsers(User updateUser, Long id) {
+    public boolean UpdateUsers(UserRequests updatedUserRequests, Long id) {
         return userRepository.findById(id)
                 .map(existingUser -> {
-                    existingUser.setFirstName(updateUser.getFirstName());
-                    existingUser.setLastName(updateUser.getLastName());
+                    updateUserFromRequest(existingUser,updatedUserRequests);
                     userRepository.save(existingUser);
                     return true;
 
@@ -35,12 +41,49 @@ public class UserServices {
     }
 
     //fetching the users
-    public List<User> fetchAllUsers() {
-
-        return userRepository.findAll();
+    public List<UserResponse> fetchAllUsers() {
+        return userRepository.findAll().stream()
+                .map(this::mapToUserResponse)
+                .collect(Collectors.toList());
     }
 
-    public Optional<User> fetchUser(Long id) {
-        return userRepository.findById(id);
+    public Optional<UserResponse> fetchUser(Long id) {
+        return userRepository.findById(id)
+                .map(this::mapToUserResponse);
+    }
+    private void updateUserFromRequest(User user, UserRequests userRequests) {
+   user.setFirstName(userRequests.getFirstName());
+   user.setLastName(userRequests.getLastName());
+   user.setEmail(userRequests.getEmail());
+   user.setPhone(userRequests.getPhone());
+   if(userRequests.getAddress() != null){
+       Address address=new Address();
+       address.setCity(userRequests.getAddress().getCity());
+       address.setStreet(userRequests.getAddress().getStreet());
+       address.setCountry(userRequests.getAddress().getCountry());
+       address.setZipcode(userRequests.getAddress().getZipcode());
+       user.setAddress(address);
+   }
+    }
+
+    private UserResponse mapToUserResponse(User user){
+        UserResponse response= new UserResponse();
+        response.setId(String.valueOf(user.getId()));
+        response.setFirstName(user.getFirstName());
+        response.setLastName(user.getLastName());
+        response.setEmail(user.getEmail());
+        response.setPhone(user.getPhone());
+        response.setRole(user.getRole());
+        if(user.getAddress()!=null){
+            AddressDTO addressDTO=new AddressDTO();
+            addressDTO.setStreet(user.getAddress().getStreet());
+            addressDTO.setCity(user.getAddress().getCity());
+            addressDTO.setCountry(user.getAddress().getCountry());
+            addressDTO.setZipcode(user.getAddress().getZipcode());
+
+        }
+     return response;
+
+
     }
 }
