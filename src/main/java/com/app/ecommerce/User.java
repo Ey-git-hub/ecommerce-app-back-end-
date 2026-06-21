f11478ec4fbccb7f06610debd190e1be9f1d490a
eyob <eyob.tadesse@astu.edu.et>
2026-06-21 11:50:53 +0300

    updated the user model

--- src/main/java/com/app/ecommerce/User.java
+++ src/main/java/com/app/ecommerce/User.java
@@ -18,5 +18,8 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    private String email;
+    private String phone;
+    private UserRole role=UserRole.CUSTOMER;
 
 }
---
+++ src/main/java/com/app/ecommerce/UserRole.java
@@ -0,0 +1,6 @@
+package com.app.ecommerce;
+
+public enum UserRole {
+CUSTOMER,ADMIN
+}
+
